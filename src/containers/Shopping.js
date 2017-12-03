e85082ad5eb265f6c4b217f1358d6a137fba3770
Mickael <dinomickael@gmail.com>
2017-12-03 14:35:18 +0000

    Add containers and route

---
+++ src/containers/Shopping.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+
+
+const mapStateToProps = (state, ownProps) => {
+  return {};
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+  };
+}
+
+const ShoppingContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)()
+
+export default ShoppingContainer;
